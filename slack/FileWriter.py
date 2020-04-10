cc872b67538fb82b69d7f7c08a456582adfe6c4d
Lawrence Ching'anda <chingl@LC-s-MacBook-Pro.local>
2020-04-10 08:41:27 +0100

    using StringIO instead of physical file

--- slack/FileWriter.py
+++ slack/FileWriter.py
@@ -1,21 +1,25 @@
-from io import IOBase, TextIOWrapper
+from io import IOBase, TextIOWrapper, StringIO
 from typing import Union, Dict
 from datetime import datetime
-#from whatsapp.Line import Line
+from whatsapp.Line import Line
 
 class FileWriter(object):
 
-	def __init__(self, fileHandle:Union[TextIOWrapper,str], channel:str=None, delimiter:str=None, overrideUsername:bool=None):
+	FORMAT_STRING:str = '{1}{0} "{2}"{0} "@{3}"{0} "{4}"'
+
+	def __init__(self, fileHandle:Union[StringIO,TextIOWrapper,str], channel:str=None, delimiter:str=None, overrideUsername:bool=None):
 
 		try:
 			if isinstance(fileHandle, TextIOWrapper):
 				if (fileHandle.mode.count("w")) == 0:
-					raise FileNotFoundError("File needs to be in write mode")
+					raise IOError("File needs to be in write mode")
 				self._file:TextIOWrapper = fileHandle
 			elif isinstance(fileHandle, str):
 				self._file:TextIOWrapper = open(fileHandle, "w")
+			elif isinstance(fileHandle, StringIO):
+				self._file:TextIOWrapper = fileHandle
 			else:
-				raise TypeError("Only type <File> or <string> are allowed")
+				raise TypeError("Only type <File> or <string> or <StringIO> are allowed")
 		except TypeError as error:
 			raise error
 
@@ -34,19 +38,22 @@ class FileWriter(object):
 	def setOverrideUsername(self, override:bool):
 		self._overrideUsername = bool(override)
 
-	def write(self, line:Dict[str,Union[str,datetime]]):
-#		new_line:str = FORMAT_STRING.format(self._delimiter, 
-#						int(line["date"].timestamp()), 
-#						self._channel, 
-#						line["username"], 
-#						line["content"])
-#		print(new_line)
-#		outfile.write(new_line+"\n")
-		output_elements = {}
-#		return None
+	def write(self, line:Line):
 
+		new_line:str = self.FORMAT_STRING.format(self._delimiter,
+						int(line.getDate().timestamp()),
+						self._channel,
+						line.getUsername(),
+						line.getContent())
+
+		self._file.write(new_line+"\n")
+
+
+"""
 import pathlib
 import sys
+
+print(__name__)
 
 path = str(pathlib.Path(__file__).parent.absolute())
 print(path)
@@ -58,3 +65,4 @@ fileWriter.setOverrideUsername(True)
 print(fileWriter._overrideUsername)
 
 print(pathlib.Path(__file__).home())
+"""
---
+++ tests/slack/FileWriter_test.py
@@ -0,0 +1,37 @@
+from slack.FileWriter import FileWriter
+from whatsapp.Line import Line
+from datetime import datetime
+import pathlib, hashlib
+from pytest import raises
+from io import StringIO
+
+
+def test_createFileWriter():
+
+	path = str(pathlib.Path(__file__).parent.absolute())
+
+	output_file = StringIO()
+	fixture_file = open(path + "/slack_test_fixture.csv", "rb")
+
+	fileWriter:FileWriter = FileWriter(output_file, delimiter="!", channel="test")
+
+	fileWriter.write(Line(datetime(2019, 4, 13), "username", "content"))
+
+	hash_reader = hashlib.md5()
+	hash_reader.update(output_file.getvalue().encode("utf-8"))
+	output_hash = hash_reader.hexdigest()
+
+	hash_reader = hashlib.md5()
+	hash_reader.update(fixture_file.read())
+	fixture_hash = hash_reader.hexdigest()
+
+	assert output_hash == fixture_hash
+
+def test_fileNotInWriteMode():
+
+	path = str(pathlib.Path(__file__).parent.absolute())
+
+	output_file = open(path + "/slack_test_fixture.csv", "r")
+
+	with raises(IOError):
+		fileWriter:FileWriter = FileWriter(output_file)
