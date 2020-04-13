69033083ee15f56751418656cb6682b74858a296
Lawrence Ching'anda <chingl@LC-s-MacBook-Pro.local>
2020-04-13 11:14:46 +0100

    adding validateInteger and tests

--- tests/utilities/ParameterValidator_test.py
+++ tests/utilities/ParameterValidator_test.py
@@ -1,4 +1,5 @@
 from utilities.ParameterValidator import ParameterValidator
+import pytest
 
 def test_validateStringWithValue():
 
@@ -9,31 +10,67 @@ def test_validateStringWithValue():
 
 	assert parameter == validated
 
-def test_validateStringWithNoValue():
+@pytest.mark.parametrize("parameter, expected", 
+							[("", None), 
+							("\n  \r", None),
+							("   ", None),
+							(None, None)])
+def test_validateStringWithNoValue(parameter, expected):
 
 	validator:ParameterValidator = ParameterValidator()
 
-	parameter:str = ""
 	validated:str = validator.validateString(parameter)
 
-	assert None == validated
+	assert expected == validated
 
-	parameter:str = None
-	validated:str = validator.validateString(parameter)
-
-	assert None == validated
-
-def test_validateStringWithDefault():
+@pytest.mark.parametrize("parameter, default, expected", 
+							[("", 10, "10"), 
+							(None, None, None),
+							("", None, None)])
+def test_validateStringWithDefault(parameter, default, expected):
 
 	validator:ParameterValidator = ParameterValidator()
 
-	parameter:str = ""
-	default:str = 10
 	validated:str = validator.validateString(parameter, default)
 
-	assert str(default) == validated
+	assert expected == validated
 
-	default:str = None
-	validated:str = validator.validateString(parameter, default)
+def test_validateIntegerWithValue():
 
-	assert None == validated 
+	validator:ParameterValidator = ParameterValidator()
+
+	parameter:int = 10
+	validated:str = validator.validateInteger(parameter)
+
+	assert parameter == validated
+
+
+@pytest.mark.parametrize("parameter, expected", 
+							[("", None), 
+							("\n  \r", None),
+							(10.4, None),
+							(None, None)])
+def test_validateIntegerWithNoValue(parameter, expected):
+
+	validator:ParameterValidator = ParameterValidator()
+
+	validated:int = validator.validateInteger(parameter)
+
+	assert expected == validated
+
+
+@pytest.mark.parametrize("parameter, default, expected", 
+							[("", 10, 10),
+							("15", "23", 23),
+							("", 23.3, 23),
+							("rfdfdsf", "23.3", None),
+							("", "test", None),  
+							(None, None, None),
+							("", None, None)])
+def test_validateIntegerWithDefault(parameter, default, expected):
+
+	validator:ParameterValidator = ParameterValidator()
+
+	validated:int = validator.validateInteger(parameter, default)
+
+	assert expected == validated
--- utilities/ParameterValidator.py
+++ utilities/ParameterValidator.py
@@ -10,6 +10,16 @@ class ParameterValidator(object):
 				
 		string = string if (string is not None 
 									and type(string) == str 
-									and len(string) > 0) else (default if default is None else str(default))
+									and len(string.strip()) > 0) else (default if default is None else str(default))
 
 		return string
+
+	def validateInteger(self, integer:int, default:int=None) -> str:
+		
+		try:
+			integer = integer if (integer is not None 
+										and type(integer) == int) else (default if default is None else int(default))
+		except ValueError:
+			integer = None
+
+		return integer
