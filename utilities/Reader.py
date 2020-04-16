2da022a0fb424d70beff815af9580f4fbc78df3e
Lawrence Ching'anda <chingl@LC-s-MacBook-Pro.local>
2020-04-16 16:47:12 +0100

    adding ffile method to Reader

--- utilities/Reader.py
+++ utilities/Reader.py
@@ -1,10 +1,15 @@
 import abc
+from io import BufferedReader
 
 from utilities.Line import Line
 from utilities.Writer import Writer
 
 
 class Reader(abc.ABC):
+
+	@abc.abstractmethod
+	def file(self) -> BufferedReader:
+		pass
 
 	@abc.abstractmethod
 	def read(self) -> Line:
